3851b305ef28a186b677a54636c8ab4ea18abc28
John Coggeshall <john@coggeshall.org>
2016-11-26 18:41:19 -0500

    Fixing HTML

--- resources/views/recipe/mirror/top_right.blade.php
+++ resources/views/recipe/mirror/top_right.blade.php
@@ -1,6 +1,6 @@
 <table style="width:550px">
     <tr>
-        <td align="center" class="bright" style="border-bottom: thin solid white;">Ingredients<br/><span class="small">({{ $recipe->servings }} servings)</span></td>
+        <td align="center" colspan="3" class="bright" style="border-bottom: thin solid white;">Ingredients<br/><span class="small">({{ $recipe->servings }} servings)</span></td>
     </tr>
     @foreach($recipe->ingredients as $ingredient)
         <tr>
